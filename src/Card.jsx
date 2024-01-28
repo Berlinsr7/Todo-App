cbd032518393e6b6bb023be4c8b1f0d6b2da3f79
unknown <berlinsam190100@gmail.com>
2024-01-28 23:16:17 +0530

    Initial commit

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,16 +2,8 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
 
 
-function Card({k,data,Delete,Edit,onComplete}) {
-
-    function handleDelete(){
-        Delete(data);
-    }
-
-    function handleEdit(){
-        Edit(data)
-    }
-    
+function Card({k,data,Delete,Edit,onComplete}) 
+{    
   return (
     <div className='col-lg-4 col-md-6' style={{fontFamily: "Bebas Neue"}}>
         <div className="card shadow p-3 mb-4" style={{backgroundColor:'rgb(250, 250, 210)'}}>
@@ -24,8 +16,8 @@ function Card({k,data,Delete,Edit,onComplete}) {
             </button>
         </div>
         <div className="text-end me-5 mt-5">
-            <button className="btn btn-success me-2" onClick={() => handleEdit()}>Edit</button>
-            <button className="btn btn-danger" onClick={()=> handleDelete()}>Delete</button>
+            <button className="btn btn-success me-2" onClick={() => Edit(data)}>Edit</button>
+            <button className="btn btn-danger" onClick={()=> Delete(data)}>Delete</button>
         </div>
         </div>
     </div>
